01dcd4ad797aa96cd142afdbee6336d22e5b078c
Osvaldo Holanda <osvaldolimah@gmail.com>
2025-08-21 21:08:04 -0300

    Update BonusAccount.ts

--- class/BonusAccount.ts
+++ class/BonusAccount.ts
@@ -1,1 +1,12 @@
+import { DioAccount } from './DioAccount';
 
+export class BonusAccount extends DioAccount {
+    deposit = (value: number): void => {
+        // Acessando o método privado via this['validateStatus']()
+        if (this['validateStatus']()) {
+            const bonus = 10;
+            this.balance += value + bonus;
+            console.log(`Depósito de ${value} realizado com bônus de ${bonus}. Saldo atual: ${this.balance}`);
+        }
+    }
+}
