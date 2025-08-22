c99a8029eaa86b71661cc3bd504a79769f222ddd
Osvaldo Holanda <osvaldolimah@gmail.com>
2025-08-21 21:05:17 -0300

    Update CompanyAccount.ts

--- class/CompanyAccount.ts
+++ class/CompanyAccount.ts
@@ -1,12 +1,12 @@
-import { DioAccount } from "./DioAccount"
+import { DioAccount } from './DioAccount';
 
 export class CompanyAccount extends DioAccount {
-
-  constructor(name: string, accountNumber: number){
-    super(name, accountNumber)
-  }
-
-  getLoan = (): void => {
-    console.log('Voce pegou um empréstimo')
-  }
+    getLoan = (value: number): void => {
+        if (this.validateStatus()) {
+            this.balance += value;
+            console.log(`Empréstimo de ${value} aprovado. Saldo atual: ${this.balance}`);
+        } else {
+            console.log('Conta inativa. Empréstimo não permitido.');
+        }
+    }
 }
