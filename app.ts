52749e25713ec61b39b72fe4579670aadd140f07
Osvaldo Holanda <osvaldolimah@gmail.com>
2025-08-21 21:08:32 -0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,9 +1,20 @@
-import { CompanyAccount } from './class/CompanyAccount'
-import { PeopleAccount } from './class/PeopleAccount'
+import { CompanyAccount } from './class/CompanyAccount';
+import { PeopleAccount } from './class/PeopleAccount';
+import { BonusAccount } from './class/BonusAccount';
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+const companyAccount: CompanyAccount = new CompanyAccount("Empresa X", 654321);
+companyAccount.deposit(1000);
+
+const peopleAccount: PeopleAccount = new PeopleAccount("Joao", 123456, 98765432100);
+console.log(peopleAccount);
+/* peopleAccount.setName("Maria");
+
+peopleAccount.deposit(500);
+console.log(peopleAccount);
+console.log(peopleAccount.getName()); */
+peopleAccount.deposit(500);
+
+const contaBonus = new BonusAccount('Maria', 12345);
+
+contaBonus.deposit(100); // Deve adicionar 110 ao saldo
+contaBonus.getBalance();
